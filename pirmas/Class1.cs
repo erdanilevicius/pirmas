223bde8966b7a4ccd1a0870e3fc756f6f51a002a
steam <steam@ERNESTAS>
2022-03-04 19:23:26 +0200

    Add project files.

---
+++ pirmas/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzd1
+{
+
+    class uzduotis {
+        public double c;
+        public int i;
+
+        public static void daryti() {
+
+            double daug = Math.Pow(10, 15);
+            double a = 1;
+            double b = 2;
+            double c = 0;
+            int i = 0;
+
+            while (c <= daug) {
+                c = a * b;
+                a = b;
+                b = c;
+                i++;
+            }
+
+            Console.WriteLine("Ats.: " + c);
+            Console.WriteLine("Prasukta: " + i + " kartus.");
+
+        }
+        
+
+    }
+
+}
+       
+        
---
+++ pirmas/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//nebūtina
+using System;
+using static Uzd1.uzduotis; // naudojama klases faile esanti klase
+//nebūtina
+namespace Uzd
+{
+    class Programa
+    {
+        public static void Main(string[] args)
+        {
+
+            Uzd1.uzduotis myObj = new Uzd1.uzduotis();
+
+            daryti(); // kvieciama ir vykdoma funkcija daryti esanti "Class1" faile
+
+            Console.WriteLine("Ats.: " + myObj.c);  // nepasiima kintamuju
+            Console.WriteLine("Prasukta: " +myObj.i + " kartus.");
+
+        }
+
+    }
+}
